a478b74ba6b297c7c27b79b4420adfd8625e2fa9
linder3hs <linderhassinger00@gmail.com>
2022-06-24 22:58:30 -0500

    funciones

--- semana4/js/dia5-2.js
+++ semana4/js/dia5-2.js
@@ -59,5 +59,74 @@ const dividir = (n1, n2) => {
 // Recordemos que isNaN retornar un boolena por ende esta
 // funcion retora true o false
 function validarSiEsNumero(numero) {
+  //isNaN is not a number
+  // Si no es un numero (numero)
   return !isNaN(numero);
 }
+
+// De forma global vamos a crear un array de alumnos
+// Esto es un array vacio
+const alumnos = [];
+// alumnos = ["Juan", "Pepe"]
+
+const imprimirAlumnos = () => {
+  const inputNombre = document.querySelector("#input_name");
+  const nombre = inputNombre.value;
+  // Estamos agregando el alumnos al array
+  alumnos.push(nombre);
+  // Estamos indicando el value del input es igual cuando hacemos estos
+  // estamos limpiado el contenido del input
+  inputNombre.value = "";
+  const resultado = document.querySelector(".resultado");
+
+  // Siempre debemos limpiar el espacio del resultado antes del for
+  resultado.innerHTML = "";
+
+  for (let alumno of alumnos) {
+    // += es adicion es una suma la
+    //  resultado.innerHTML = resultado.innerHTML + "<p>" + alumno + "</p>";
+    // Indica que al contenido anterior le agremos esto si yo no uso el +=
+    // va a reemplazar el contenido
+    resultado.innerHTML += "<p>" + alumno + "</p>";
+  }
+};
+
+//Registrar el ingreso de notas de 4 examenes y calcular el promedio de estos.
+// Funcion que reciba 4 parametros valide si son numeros y si todo esta ok que retorne el promedio
+
+// los valores pueden por inputs o por prompt
+const calcularPromedio = () => {
+  const nota1 = Number(document.querySelector("#input_nota1").value);
+  const nota2 = Number(document.querySelector("#input_nota2").value);
+  const nota3 = Number(document.querySelector("#input_nota3").value);
+  const nota4 = Number(document.querySelector("#input_nota4").value);
+  const resultado2 = document.querySelector(".resultado2");
+
+  if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4)) {
+    alert("Ingrese numeros validos");
+    return;
+  }
+
+  if (nota1 > 20) {
+    alert("La nota 1 no debe ser mayor a 20");
+    return;
+  }
+
+  if (nota2 > 20) {
+    alert("La nota 2 no debe ser mayor a 20");
+    return;
+  }
+
+  if (nota3 > 20) {
+    alert("La nota 3 no debe ser mayor a 20");
+    return;
+  }
+
+  if (nota4 > 20) {
+    alert("La nota 4 no debe ser mayor a 20");
+    return;
+  }
+
+  const promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+  resultado2.innerHTML = "<p>El pomedio es: " + promedio + "</p>";
+};
